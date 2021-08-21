2023c9c3eb31edfd7682bf608cda89168a4cd27a
ohgeejoe <seoung.shin@ontariotechu.net>
2021-08-21 02:37:39 +0000

    Initial commit with all my javascripting work so far

---
+++ function-arguments.js
@@ -0,0 +1,8 @@
+function math (arg1,arg2,arg3) {
+    let x = arg2 * arg3
+    let y = arg1 + x
+    return y
+}
+
+console.log(math(53,61,67));
+
---
+++ looping-through-arrays.js
@@ -0,0 +1,10 @@
+let pets = ['cat','dog','rat'];
+let petsLength = pets.length;
+
+for (let i=0;i < petsLength;i++) {
+    pets[i] = pets[i] + 's'
+}
+
+
+
+console.log(pets);
